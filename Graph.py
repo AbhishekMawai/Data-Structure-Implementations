87dc0a1501e84a41bdbe6ac2e08cf245eeedfd90
Abhishek Mawai <abhishek_mawai@hotmail.com>
2017-06-03 18:36:44 +0530

    Add Graph DS implementation

---
+++ Graph.py
@@ -0,0 +1,126 @@
+graph = {"a": ["c", "f"],
+         "b": ["c", "e"],
+         "c": ["a", "b", "d", "e"],
+         "d": ["c"],
+         "e": ["c", "b"],
+         "f": ["d"],
+         "k": []
+         }
+
+
+class Graph(object):
+
+    def __init__(self, graph_dict=None):
+
+        if graph_dict is None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
+
+    def vertices(self):
+        return list(self.__graph_dict.keys())
+
+    def edges(self):
+        edges = []
+        for node in self.__graph_dict:
+            for neighbor in self.__graph_dict[node]:
+                edges.append((node, neighbor))
+        return edges
+
+    def find_isolated_nodes(graph):
+        edges = []
+        for node in graph:
+            if not graph[node]:
+                edges.append(node)
+        return edges
+
+    def add_vertex(self, vertex):
+
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        """ assumes that edge is of type set, tuple or list;
+            between two vertices can be multiple edges!
+        """
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def __generate_edges(self):
+        """ A static method generating the edges of the
+            graph "graph". Edges are represented as sets
+            with one (a loop back to the vertex) or two
+            vertices.
+        """
+        edges = []
+        for vertex in self.__graph_dict:
+            for neighbour in self.__graph_dict[vertex]:
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+        return edges
+
+    def is_connected(self,
+                     vertices_encountered=None,
+                     start_vertex=None):
+        """ determines if the graph is connected """
+        if vertices_encountered is None:
+            vertices_encountered = set()
+        gdict = self.__graph_dict
+        vertices = list(gdict.keys())  # "list" necessary in Python 3
+        if not start_vertex:
+            # chosse a vertex from graph as a starting point
+            start_vertex = vertices[0]
+        vertices_encountered.add(start_vertex)
+        if len(vertices_encountered) != len(vertices):
+            for vertex in gdict[start_vertex]:
+                if vertex not in vertices_encountered:
+                    if self.is_connected(vertices_encountered, vertex):
+                        return True
+        else:
+            return True
+        return False
+
+    def find_path(self, start_vertex, end_vertex, path=[]):
+        """ find a path from start_vertex to end_vertex
+            in graph """
+        graph = self.__graph_dict
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return path
+
+        if start_vertex not in graph:
+            return None
+
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_path = self.find_path(vertex,
+                                               end_vertex,
+                                               path)
+                if extended_path:
+                    return extended_path
+        return None
+
+    def find_all_paths(self, start_vertex, end_vertex, path=[]):
+        """ find all paths from start_vertex to
+            end_vertex in graph """
+        graph = self.__graph_dict
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return [path]
+        if start_vertex not in graph:
+            return []
+        paths = []
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_paths = self.find_all_paths(vertex,
+                                                     end_vertex,
+                                                     path)
+                for p in extended_paths:
+                    paths.append(p)
+        return paths
+
+
+g = Graph(graph)
